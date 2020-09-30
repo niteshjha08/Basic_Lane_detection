355cab608e7203e4ed9892e4ba679fb6ca9f7610
Nitesh Jha <niteshj818@gmail.com>
2020-10-01 01:24:39 +0530

    added tuner and finished tuning for region and color_thresholds

--- lane_finding.py
+++ lane_finding.py
@@ -8,12 +8,31 @@ while(cap.isOpened()):
     ret,image=cap.read()
     if(ret==True):
         img=image.copy()
+        hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
         # COLOR THRESHOLDING
-        red_thresh=200
-        green_thresh=200
-        blue_thresh=200
+        # lower_white=np.array([0,0,200],dtype=np.uint8)
+        # upper_white=np.array([255,30,255],dtype=np.uint8)
+        # whites_mask=cv2.inRange(hsv,lower_white,upper_white)
+        #
+        # lower_yellow=np.array([15,127,127],dtype=np.uint8)
+        # upper_yellow=np.array([100,255,255],dtype=np.uint8)
+        # yellows_mask=cv2.inRange(hsv,lower_yellow,upper_yellow)
+        # agg_mask=cv2.bitwise_or(whites_mask,yellows_mask)
+        # cv2.imshow('agg',agg_mask)
+        # cv2.imshow('yellows',yellows_mask)
+        # cv2.imshow('whites',whites_mask)
+        # canny=cv2.Canny(gray,50,150)
+        # cv2.imshow('canny',canny)
+        # edges=cv2.bitwise_and(canny,agg_mask)
+        # cv2.imshow('filter',edges)
+        # cv2.waitKey()
+
+
+        red_thresh=220
+        green_thresh=180
+        blue_thresh=70
         rgb_thresh=[blue_thresh,green_thresh,red_thresh]
 
         color_threshold=(img[:,:,0]<rgb_thresh[0]) | (img[:,:,1]<rgb_thresh[1]) | (img[:,:,2]<rgb_thresh[2])
@@ -25,8 +44,8 @@ while(cap.isOpened()):
         # REGION MASKING
         shape=img.shape
 
-        right_coordinates=np.array([[(480,shape[0]),(480,320),(640,320),(shape[1],shape[0])]],dtype=np.int32)
-        left_coordinates=np.array([[(0,shape[0]),(320,320),(480,320),(480,shape[0])]],dtype=np.int32)
+        right_coordinates=np.array([[(480,shape[0]),(480,320),(600,320),(shape[1],shape[0])]],dtype=np.int32)
+        left_coordinates=np.array([[(0,shape[0]),(360,320),(480,320),(480,shape[0])]],dtype=np.int32)
         blackl=gray.copy()*0
         blackr=blackl.copy()
         left_mask=cv2.fillPoly(blackl,left_coordinates,255)
@@ -35,8 +54,8 @@ while(cap.isOpened()):
         # FITTING SLOPES OF LEFT AND RIGHT LINES
         roi_left=cv2.bitwise_and(canny,left_mask)
         roi_right=cv2.bitwise_and(canny,right_mask)
-        left_lines=cv2.HoughLinesP(roi_left,2,math.pi/180,5,np.array([]),20,50)
-        right_lines=cv2.HoughLinesP(roi_right,2,math.pi/180,5,np.array([]),20,50)
+        left_lines=cv2.HoughLinesP(roi_left,2,math.pi/180,5,np.array([]),20,40)
+        right_lines=cv2.HoughLinesP(roi_right,2,math.pi/180,5,np.array([]),20,40)
 
         xs=list()
         ys=list()
@@ -65,6 +84,7 @@ while(cap.isOpened()):
 
         final=cv2.addWeighted(image,0.6,copy,0.4,0)
         cv2.imshow('frame',final)
+        cv2.imshow('leftroi',roi_left)
 
         cv2.waitKey(33)
     else:
---
+++ lane_finding_tuner.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import math
+
+#cap=cv2.VideoCapture('test_videos/solidWhiteRight.mp4')
+
+
+#    ret,image=cap.read()
+#    if(ret==True):
+def nothing(x):
+    pass
+cv2.namedWindow('Tuner')
+cv2.createTrackbar('Red_threshold','Tuner',0,255,nothing)
+cv2.createTrackbar('Green_threshold', 'Tuner',0, 255, nothing)
+cv2.createTrackbar('Blue_threshold', 'Tuner', 0 ,255, nothing)
+while(cv2.waitKey(10)!=ord('q')):
+    image=cv2.imread('test_images/solidYellowLeft.jpg')
+    img=image.copy()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # COLOR THRESHOLDING
+    # red_thresh= 202
+    # green_thresh= 195
+    # blue_thresh= 200
+
+
+    red_thresh=cv2.getTrackbarPos('Red_threshold','Tuner')
+    green_thresh = cv2.getTrackbarPos('Green_threshold', 'Tuner')
+    blue_thresh = cv2.getTrackbarPos('Blue_threshold', 'Tuner')
+    rgb_thresh=[blue_thresh,green_thresh,red_thresh]
+
+    color_threshold=(img[:,:,0]<rgb_thresh[0]) | (img[:,:,1]<rgb_thresh[1]) | (img[:,:,2]<rgb_thresh[2])
+
+    img[color_threshold]=[0,0,0]
+    cv2.imshow('thresholded img',img)
+    gray[color_threshold]=0
+    canny=cv2.Canny(gray,50,150)
+
+    # REGION MASKING
+    shape=img.shape
+
+    right_coordinates=np.array([[(480,shape[0]),(480,320),(640,320),(shape[1],shape[0])]],dtype=np.int32)
+    left_coordinates=np.array([[(0,shape[0]),(320,320),(480,320),(480,shape[0])]],dtype=np.int32)
+    blackl=gray.copy()*0
+    blackr=blackl.copy()
+    left_mask=cv2.fillPoly(blackl,left_coordinates,255)
+    right_mask=cv2.fillPoly(blackr,right_coordinates,255)
+
+    # FITTING SLOPES OF LEFT AND RIGHT LINES
+    roi_left=cv2.bitwise_and(canny,left_mask)
+    roi_right=cv2.bitwise_and(canny,right_mask)
+    cv2.imshow('roi_left',roi_left)
+    left_lines=cv2.HoughLinesP(roi_left,2,math.pi/180,5,np.array([]),20,50)
+    right_lines=cv2.HoughLinesP(roi_right,2,math.pi/180,5,np.array([]),20,50)
+
+    xs=list()
+    ys=list()
+
+    copy = image.copy()
+    try:
+        for left in left_lines:
+            for (x1,y1,x2,y2) in left:
+                xs.extend([x1,x2])
+                ys.extend([y1,y2])
+        A,B=np.polyfit(xs,ys,1)
+        start=(int((shape[0]-B)/A),shape[0])
+        end=(int((325-B)/A),325)
+        cv2.line(image,start,end,(0,0,255),10)
+    except:
+        print('LEFT LINES EMPTY!')
+    xs=list()
+    ys=list()
+    try:
+        for right in right_lines:
+            for (x1,y1,x2,y2) in right:
+                xs.extend([x1,x2])
+                ys.extend([y1,y2])
+        A,B=np.polyfit(xs,ys,1)
+        start=(int((shape[0]-B)/A),shape[0])
+        end=(int((325-B)/A),325)
+        cv2.line(image,start,end,(0,0,255),10)
+    except:
+        print('RIGHT LINES EMPTY!')
+
+
+    final=cv2.addWeighted(image,0.6,copy,0.4,0)
+    cv2.imshow('frame',final)
+
+
